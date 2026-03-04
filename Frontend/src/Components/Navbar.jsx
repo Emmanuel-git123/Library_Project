7324dbd8737af36773fad460b83c0f58247254d7
Muni-27 <munivenkat9999@gmail.com>
2026-03-04 22:08:48 +0530

    some

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -40,13 +40,14 @@ const Navbar = () => {
                     <Link to="/view/author" className='px-3 py-3 transition-all duration-300 transform hover:scale-150 font-bold text-white text-xs hover:text-orange-400 hover:cursor-pointer flex items-center justify-center '>Authors</Link>
                     <Link to="/view/supervisor" className='px-3 py-3 transition-all duration-300 transform hover:scale-150 font-bold text-white text-xs hover:text-orange-400 hover:cursor-pointer flex items-center justify-center '>Supervisors</Link>
                     <Link to="/view/thesis_type" className='px-3 py-3 transition-all duration-300 transform hover:scale-150 font-bold text-white text-xs hover:text-orange-400 hover:cursor-pointer flex items-center justify-center '>Thesis Type</Link>
-                    {token&&<Link to='/view/upload' className='px-3 py-3 transition-all duration-300 transform hover:scale-150 font-bold text-white text-xs hover:text-orange-400 hover:cursor-pointer flex items-center justify-center'>Upload Thesis</Link>}                </div>
+                    {token&&<Link to='/view/upload' className='px-3 py-3 transition-all duration-300 transform hover:scale-150 font-bold text-white text-xs hover:text-orange-400 hover:cursor-pointer flex items-center justify-center'>Upload Thesis</Link>}                
+                </div>
                 <div className='border border-white px-3 flex justify-between bg-gray-300 py-0.5'>
                     <div className='flex gap-1 items-center'>
                         {token?
                         <Link to="/" onClick={handleLogOut} className='underline text-indigo-700 text-sm hover:text-red-500'>Log out</Link>
                         // :<Link to="/login" className='underline text-indigo-700 text-sm hover:text-red-500'>Login</Link>}\
-                        :<Link to="/login" className='group relative overflow-hidden animate-pulse text-sm w-36 bg-gray-800 h-6 text-center border-0 rounded-2xl text-white opacity-500 hover:bg-gray-900 focus:outline-4 focus-outline-offset-2 focus:outline-black'>
+                        :<Link to="/login" className='group relative overflow-hidden text-sm w-36 bg-gray-800 h-6 text-center border-0 rounded-2xl text-white opacity-500 hover:bg-gray-900 focus:outline-4 focus-outline-offset-2 focus:outline-black'>
                             <span className='absolute inset-0 bg-white opacity-30 scale-0 group-hover:scale-200 transition-transform duration-1000 rounded-2xl w-36'></span>
                             <span className='relative z-10'>Login</span>
                         </Link>}
--- Frontend/src/Pages/About_Us.jsx
+++ Frontend/src/Pages/About_Us.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Librarypic from '../assets/library.jpg'
+import { animate } from 'animejs';
+
 
 const About_Us = () => {
+    animate('.square', { x: '10rem' });
+    animate('#css-selector-id', { rotate: '1turn' });
+    animate('.row:nth-child(3) .square', { scale: [1, .5, 1] });
     return (
-        <div className='relative flex flex-col justify-center items-center'>
-            <img src={Librarypic} alt="librarypic" className='w-[80vw] h-auto' ></img>
+        <>
+            <div className='square w-3xl h-2xs absolute bg-amber-200 z-100'></div>
+        <div className='relative flex flex-col justify-center items-center min-h-screen'>
             <div className='flex flex-col text-center absolute justify-center items-center w-[80vw] max-h-[60vh] px-10 z-10 bg-blue-50'>
                 <div className='font-semibold text-lg text-gray-600 mb-2 text-center'>About the Repository</div>
                 <div className='text-xs font-500 mb-10'>"ethesis@nitt" is the official repository for electronic theses submitted to National Insititute of Technology Tiruchirappalli. This repository is an attempt to make all locally produced theses more visible to global users. It is mandatory for students of all courses (BTech, MTech, MTech by Research, MSc and PhD) and from all diciplines to upload the final version of the theses submitted as partial fulfillment of degree. This repository is an Open Access repository and all uploaded theses can be downloaded by any intereted user in order to use and and build upon.</div>
@@ -14,6 +20,7 @@ const About_Us = () => {
                 <div className='text-xs font-500 mb-4'>Any correspondence concerning this specific repository should be sent to <a href="/" className='text-indigo-700 underline'>bpcl-dig@nitt.ac.in</a></div>
             </div>
         </div>
+        </>
     )
 }
 
